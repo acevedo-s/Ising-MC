6c219d34115c6467e5294bb3b9034b8492965888
acevedo-s <santiagodanielacevedo@gmail.com>
2024-02-02 14:13:55 +0100

    .

---
+++ 1D/single/runmain.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=IsingMC
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=48:00:00
+#SBATCH --mem=8G
+#SBATCH --partition=long2,long1
+#SBATCH --output=./log_output/%x.o%j              # Standard output
+#SBATCH --error=./log_output/%x.o%j               # Standard error
+
+python3 main.py

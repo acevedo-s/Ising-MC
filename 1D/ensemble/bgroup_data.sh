810e1bbde8364f710bf4548136b36b0be38e6d6e
acevedo-s <santiagodanielacevedo@gmail.com>
2024-07-17 13:02:40 +0200

    .

--- 1D/ensemble/bgroup_data.sh
+++ 1D/ensemble/bgroup_data.sh
@@ -4,10 +4,10 @@ mkdir -p log_group
 
 n_seeds=$1
 L=$2
-Nrealizations=49
-r_idmin=45
+r_idmax=22
+r_idmin=44
 
-for (( r_id=r_idmin; r_id<Nrealizations; r_id++ ))
+for (( r_id=r_idmin; r_id<=r_idmax; r_id++ ))
 do
   sbatch sgroup_data.sh "$n_seeds" "$L" "$r_id"
   sleep 0.01
--- 1D/ensemble/error_correcting_code.sh
+++ 1D/ensemble/error_correcting_code.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 L=10000
-seed_id0=2501
+seed_id0=1
 seed_id1=5000
 n_seeds=5000
 
-r_idmin=47
-r_idmax=75
-for (( r_id=r_idmin+1; r_id<r_idmax; r_id++ ))
+r_idmin=45
+r_idmax=45
+for (( r_id=r_idmin; r_id<=r_idmax; r_id++ ))
 do
   for (( seed_id=seed_id0; seed_id<=seed_id1; seed_id++ ))
   do
@@ -14,8 +14,12 @@ do
     filename="/scratch/sacevedo/Ising-Chain/canonical/L$L/r_id$r_id/seed$seed/"
     if [ -z "$(ls -A $filename)" ]
     then
-      sbatch smain.sh "$seed" "$L" "$r_id"
+      # sbatch smain.sh "$seed" "$L" "$r_id"
       # echo $seed
+      DEPENDENCY_ID=$(sbatch smain.sh "$seed" "$L" "$r_id" | awk '{print $4}')
     fi
   done
 done
+
+
+echo this took: $((SECONDS/60 )) m
